586d1ead6b0c9cf71c73c8011e874830e46c9b1b
Sarthak Sachdev <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-10 11:20:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
 window.onload = (event) => {
     console.log("page is fully loaded");
-  
-      function hide(){
-    const object_d = document.querySelector('.hov');
-       object_d.style.visibility = 'hidden' ; 
-      }
-      function show(){
-         const object_d = document.querySelector('.hov');
-         object_d.style.visibility = 'visible';
-      }
-      document.querySelector('.img-hover').addEventListener('mouseenter', show);
-      document.querySelector('.img-hover').addEventListener('mouseleave', hide);
-  };
